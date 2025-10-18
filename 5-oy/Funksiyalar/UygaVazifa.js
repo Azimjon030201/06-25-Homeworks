10588bebcd128dc6b21120b8484418a66232f26f
faridaxakimova2-sudo <faridaxakimova2@gmail.com>
2025-10-18 16:15:09 +0500

    uyga vazifa qildim 18.10.25

---
+++ 5-oy/Funksiyalar/UygaVazifa.js
@@ -0,0 +1,34 @@
+// N - 1: GetLength(str).
+// function getLength(str) {
+//     return str.length;
+//   }
+//   console.log(getLength("JavaScript")); 
+// N - 2 Find Max(arr).
+// function findMax(arr ){
+//     arr = [2,8,5,10];
+//     return Math.max(arr);
+// }
+// console.log(findMax(arr));
+  // N - 3 Reverse String(str)
+//   function reverseString (str){
+//     return str.split('').reverse().join('');
+//   }
+//   console.log(reverseString("Salom"));
+//  N - 5  checkNumber
+// function checkNumber(num ){
+//     if (num>0)return "Musbat";
+//     else if(num < 0) return "Manfiy";
+//     else return "Nol"
+// }
+// console.log(checkNumber(-5));
+//  N -6 Karkulyator
+// function karkulyatorYosh(TugilganYil,HozirgiYil){
+//     return HozirgiYil - TugilganYil ;
+// }
+// console.log(karkulyatorYosh(1995,2025));
+// N - 7 
+// function countWords(sentence){
+//     return sentence.trim().split(/\s+/).length;
+// }
+// console.log( countWords("Men bugun darsga bordim"));
+
---
+++ 5-oy/Funksiyalar/index.js
@@ -0,0 +1,36 @@
+// function hello (ism){
+//     console.log("Hello ",ism);
+// }
+// hello( "Nargiza");
+// hello ("Sug'diyona");
+// function Sum(a,b){
+//     return a + b
+//     a * b
+// }
+// let s = Sum(5,6);
+// console.log(s);
+///////////////////////
+// let arr = [5,8,12,7,3,18]
+// function SumArr(array){
+//     let result = 0
+//     for (const item of array){
+//         result = result + item *2
+//     }
+//     return result
+// }
+// console.log(SumArr(arr));
+////////////////////////
+// function darajaUch(a){
+    
+//     return  a **3;
+// }
+// let raqam  = 5
+// console.log("natija " +darajaUch (raqam));
+// function daraja(a){
+    
+//     return  a **2;
+// }
+// let a = 5
+// let b = 9 
+// console.log("natija " +daraja (a));
+// console.log("natija " +daraja (b));
