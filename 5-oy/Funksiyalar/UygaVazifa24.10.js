b252c44a29e8c19333ad0c992a914885e64eed70
faridaxakimova2-sudo <faridaxakimova2@gmail.com>
2025-10-24 17:31:49 +0500

    uy vazifa qildim 24.10

---
+++ 5-oy/Funksiyalar/UygaVazifa24.10.js
@@ -0,0 +1,23 @@
+//N  - 1
+// function liftHaqida() {
+//     let count = 0; 
+//     return function() {
+//       count++;
+//       return count;
+//     };
+//   }
+  
+//   const liftIshlashi = liftHaqida();
+//   console.log(liftIshlashi()); 
+//   console.log(liftIshlashi()); 
+//   console.log(liftIshlashi());
+// N - 3 
+// function VALUTA(base) {
+//     return function(num) {
+//       return base * num;
+//     };
+//   }
+  
+//   const dollarToSum = VALUTA(12000);
+//   console.log(dollarToSum(5));
+//   console.log(dollarToSum(10));
--- 5-oy/Funksiyalar/index.js
+++ 5-oy/Funksiyalar/index.js
@@ -34,3 +34,101 @@
 // let b = 9 
 // console.log("natija " +daraja (a));
 // console.log("natija " +daraja (b));
+////////20.10.2025//////////////////
+// let arr = [3,8,5,4,6,7]
+// let arr2 = arr1
+// arr1[0]=12
+// console.log(arr2);
+// function addTwo(a,b){
+//     return a + b;
+
+// }
+// let mult = (a,b)=> a*b;
+// mult(5,6)
+// let len =(str)=>str .length
+// console.log(len('Behzod'));
+// let  max =(arr)=>{
+//     let result = -Infinity
+//     for (const element of arr){
+//         if (element>result){
+//             result = element
+//         }
+//     }
+//     return result;
+// }
+// console.log(max [2,8,5,10]);
+// let a =  (parametr)=>{return}
+// let sum = (a,b)=>a +b 
+// let age = (birth,curr)=> curr- birth
+// console.log(age(2010,2025));
+// var z = 89;
+// function print(){
+ 
+//     var z = 10;
+//     console.log(z); // 10
+// }
+// print(); // 10
+// console.log(z); //
+
+// function displayZ(){
+  
+//     var z = 20;
+     
+//     {
+//         var z = 30; // Не определяет новую переменную, а изменяет значение переменной z уровня функции
+//         console.log("Block:", z);
+//     }
+//     console.log("Function:", z);
+// }
+// displayZ();
+// console.log(z);
+
+// z = 10;
+// function displayZ(){
+//     z = 20;
+//     {
+//         z = 30;
+//         console.log("Block:", z);
+//         {
+//             z=40
+//         }
+//     }
+//     console.log("Function:", z);
+// }
+// displayZ();
+// console.log("Global:", z);
+
+// function outer(){
+//     let count = 0;
+//     return function inner(){
+//         count++
+//         console.log(count);
+        
+//     }
+// }
+// let a = outer();
+// a();
+// a();
+// a();
+
+// function kopaytirish(a){
+//     return function hisobla(b){
+//         console.log(a*b)
+//     }
+// }
+// let d = kopaytirish(5)
+// d(3);
+// d(6);
+// d(2);
+
+// function outer(){
+//     let parol = "1234";
+//     function inner(p){
+//         //Logika
+//     };
+//     return inner;
+// }
+// let a = outer()
+// a("4231")
+// a("1111")
+// a("1234")
